71f3c26a31b621e6ac21980c8cb7b9a48b40b761
James Booth <jabooth@gmail.com>
2016-12-12 15:00:28 +0000

    more specific imports, tidier txt_record comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { EventEmitter } from 'events'
-import * as mdns from 'mdns'
-import * as net from 'net'
+import { createAdvertisement, Advertisement, tcp } from 'mdns'
+import { createServer, Server } from 'net'
 import * as portastic from 'portastic'
 import * as randomMac from 'random-mac'
 
@@ -9,27 +9,27 @@ import { RtspServer } from './rtsp'
 let debug = require('debug')('nodetunes:server')
 
 function geneate_txt_record(password: string | null) {
+    const pw = (password !== null) ? 'true' : 'false'
     return {
-        txtvers: '1',     // txt record version?
-        ch: '2',          // # channels
-        cn: '0,1',        // codec; 0=pcm, 1=alac, 2=aac, 3=aac elc; fwiw Sonos supports aac; pcm required for iPad+Spotify; OS X works with pcm
-        et: '0,1',        // encryption; 0=none, 1=rsa, 3=fairplay, 4=mfisap, 5=fairplay2.5; need rsa for os x
-        md: '0',          // metadata; 0=text, 1=artwork, 2=progress
-        pw: password !== null ? 'true' : 'false',    // password enabled
-        sr: '44100',          // sampling rate (e.g. 44.1KHz)
-        ss: '16',             // sample size (e.g. 16 bit?)
-        tp: 'TCP,UDP',        // transport protocol
-        vs: '105.1',          // server version?
-        am: 'AirPort4,107',   // device model
-        ek: '1',              // ? from ApEx; setting to 1 enables iTunes; seems to use ALAC regardless of 'cn' setting
-        sv: 'false',          // ? from ApEx
-        da: 'true',           // ? from ApEx
-        vn: '65537',          // ? from ApEx; maybe rsa key modulus? happens to be the same value
-        fv: '76400.10',       // ? from ApEx; maybe AirPort software version (7.6.4)
-        sf: '0x5'             // ? from ApEx
+        txtvers: '1',       // txt record version?
+        ch: '2',            // # channels
+        cn: '0,1',          // codec; 0=pcm, 1=alac, 2=aac, 3=aac elc; fwiw Sonos supports aac; pcm required for iPad+Spotify; OS X works with pcm
+        et: '0,1',          // encryption; 0=none, 1=rsa, 3=fairplay, 4=mfisap, 5=fairplay2.5; need rsa for os x
+        md: '0',            // metadata; 0=text, 1=artwork, 2=progress
+        pw: pw,             // password enabled
+        sr: '44100',        // sampling rate (e.g. 44.1KHz)
+        ss: '16',           // sample size (e.g. 16 bit?)
+        tp: 'TCP,UDP',      // transport protocol
+        vs: '105.1',        // server version?
+        am: 'AirPort4,107', // device model
+        ek: '1',            // ? from ApEx; setting to 1 enables iTunes; seems to use ALAC regardless of 'cn' setting
+        sv: 'false',        // ? from ApEx
+        da: 'true',         // ? from ApEx
+        vn: '65537',        // ? from ApEx; maybe rsa key modulus? happens to be the same value
+        fv: '76400.10',     // ? from ApEx; maybe AirPort software version (7.6.4)
+        sf: '0x5'           // ? from ApEx
     }
 }
-
 
 export interface NodeTunesOptions {
     serverName: string
@@ -57,8 +57,8 @@ export class NodeTunes extends EventEmitter {
 
     options: NodeTunesOptions
     rtspServer: RtspServer
-    netServer: net.Server | null
-    advertisement: mdns.Advertisement
+    netServer: Server | null
+    advertisement: Advertisement
 
     constructor(options: Partial<NodeTunesOptions> = {}) {
         super()
@@ -84,7 +84,7 @@ export class NodeTunes extends EventEmitter {
             retrieve: 1,
         }).then((ports: number[]) => {
             const port = ports[0]
-            this.netServer = net.createServer(this.rtspServer.connectHandler.bind(this.rtspServer))
+            this.netServer = createServer(this.rtspServer.connectHandler.bind(this.rtspServer))
 
             this.netServer.on('error', err => {
                 if (err.code == 'EADDRINUSE') {
@@ -100,7 +100,7 @@ export class NodeTunes extends EventEmitter {
             })
 
             this.netServer.listen(port, () => {
-                this.advertisement = mdns.createAdvertisement(mdns.tcp('raop'), port, {
+                this.advertisement = createAdvertisement(tcp('raop'), port, {
                     name: this.options.macAddress + '@' + this.options.serverName,
                     txtRecord: geneate_txt_record(this.options.password),
                 })
